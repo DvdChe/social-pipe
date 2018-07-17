dcaf9b17279823051b06544e13d5616fe29ebb31
David CHERRIERE <dcherriere@nbs-system.com>
2018-07-17 15:30:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/home/dch/.local/share/virtualenvs/social-pipe/bin/python
+
+import tweepy
+import sys 
+import configparser
+
+
+conf = configparser.ConfigParser()
+conf.read ('auth.conf')
+
+consumer_key        = conf['AUTH']['ConsumerKey']
+consumer_secret     = conf['AUTH']['ConsumerSecret'] 
+access_token        = conf['AUTH']['AccessToken'] 
+access_token_secret = conf['AUTH']['AccessTokenSecret'] 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+for line in sys.stdin:
+    api.update_status(line)
+
