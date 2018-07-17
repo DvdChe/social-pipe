e05e10099086999a9f4ea0e10b7ec8c024f8eb04
David CHERRIERE <dcherriere@nbs-system.com>
2018-07-17 15:38:53 +0200

    set comment

--- social-pipe.py
+++ social-pipe.py
@@ -13,12 +13,13 @@ access_token        = conf['AUTH']['AccessToken']
 access_token_secret = conf['AUTH']['AccessTokenSecret'] 
 
 
-#Connect to Twitter
+#Connect to Twitter:
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+#Send stdin to twitter :
 for line in sys.stdin:
     api.update_status(line)
 
