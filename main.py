bde061c466e94ddcb3a3b979c330750cff9aec9a
David CHERRIERE <dcherriere@nbs-system.com>
2018-07-17 15:37:09 +0200

    Clearing file

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#!/home/dch/.local/share/virtualenvs/social-pipe/bin/python
-
 import tweepy
 import sys 
 import configparser
 
 
+#Parsing conf file:
 conf = configparser.ConfigParser()
 conf.read ('auth.conf')
 
@@ -13,6 +12,8 @@ consumer_secret     = conf['AUTH']['ConsumerSecret']
 access_token        = conf['AUTH']['AccessToken'] 
 access_token_secret = conf['AUTH']['AccessTokenSecret'] 
 
+
+#Connect to Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
