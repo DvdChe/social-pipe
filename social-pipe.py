37fcc9585fd9f3c1328988b4845693a7a21bc12b
David CHERRIERE <dcherriere@nbs-system.com>
2018-08-14 16:11:33 +0200

    set logger fix

--- social-pipe.py
+++ social-pipe.py
@@ -34,7 +34,7 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(ascitime)s :: %(levelname)s :: %(message)s')
 
-file_handler = RotatingFileHandler('social-pipe.log', a, 1000000, 1)
+file_handler = RotatingFileHandler('social-pipe.log', 'a', 1000000, 1)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
