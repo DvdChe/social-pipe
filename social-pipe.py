8863ca4d318539f03c0f7e89b377abcb54ed4542
David CHERRIERE <dcherriere@nbs-system.com>
2018-08-14 17:52:48 +0200

    switching loglevel to info

--- social-pipe.py
+++ social-pipe.py
@@ -30,12 +30,12 @@ from os import path, remove
 # =====================
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
 file_handler = RotatingFileHandler('social-pipe.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
