f1e6653c158a1159a9f9e9a6c3f9deb7efd2b40a
David CHERRIERE <cherri.david@gmail.com>
2018-08-16 12:05:09 +0200

    set loglevel to debug

--- social-pipe.py
+++ social-pipe.py
@@ -30,17 +30,17 @@ from os import path, remove
 # =====================
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
 file_handler = RotatingFileHandler('social-pipe.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
 
