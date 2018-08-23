b4aa0f4dfc390637a7b7941207c607750426b847
David CHERRIERE <dcherriere@nbs-system.com>
2018-08-23 09:19:03 +0200

    Revert "improve logfile path"
    This reverts commit 4f27290f6cb340eb581164b873cc386e090faa5d.

--- social-pipe.py
+++ social-pipe.py
@@ -24,8 +24,6 @@ from logging.handlers import RotatingFileHandler
 from os import path, remove
 
 
-CurrentLocation = str(path.dirname(path.abspath(__file__)))
-
 ###############################################################################
 
 # Logging configuration
@@ -36,7 +34,7 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
-file_handler = RotatingFileHandler(CurrentLocation+'social-pipe.log', 'a', 1000000, 1)
+file_handler = RotatingFileHandler('social-pipe.log', 'a', 1000000, 1)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -49,6 +47,7 @@ logger.addHandler(file_handler)
 # General config
 # ==============
 
+CurrentLocation = str(path.dirname(path.abspath(__file__)))
 
 FlagFile = str('/tmp/social-pipe.flag')
 RetweetedHistoryFile = CurrentLocation+'/retweeted.bin'
